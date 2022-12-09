01b93875e9c3c89f0164d6b61241dc6cc104e954
Eduardo Santamaria <edsanav@gmail.com>
2022-12-09 20:50:24 +0100

    Day 9,Second part plus a bit of refactor

--- src/AOC2022/Day9.hs
+++ src/AOC2022/Day9.hs
@@ -31,22 +31,25 @@ moveHead (r, c) (L) = (r, c -1)
 moveHead (r, c) (U) = (r + 1, c)
 moveHead (r, c) (D) = (r -1, c)
 
-moveTail :: H -> T -> [Coords] -> (T, [Coords])
-moveTail h@(hRow, hCol) t@(tRow, tCol) coordsList
-  | distance2 h t <= 1 = ((tRow, tCol), coordsList)
+moveTail :: H -> T -> T
+moveTail h@(hRow, hCol) t@(tRow, tCol)
+  | distance2 h t <= 1 = (tRow, tCol)
   | hRow == tRow =
     if (hCol > tCol)
-      then let newCoords = (tRow, tCol + 1) in (newCoords, newCoords : coordsList)
-      else let newCoords = (tRow, tCol -1) in (newCoords, newCoords : coordsList)
+      then (tRow, tCol + 1)
+      else (tRow, tCol -1)
   | hCol == tCol =
     if (hRow > tRow)
-      then let newCoords = (tRow + 1, tCol) in (newCoords, newCoords : coordsList)
-      else let newCoords = (tRow -1, tCol) in (newCoords, newCoords : coordsList)
-  | hRow > tRow && hCol > tCol = let newCoords = (tRow + 1, tCol + 1) in (newCoords, newCoords : coordsList)
-  | hRow < tRow && hCol < tCol = let newCoords = (tRow -1, tCol -1) in (newCoords, newCoords : coordsList)
-  | hRow > tRow && hCol < tCol = let newCoords = (tRow + 1, tCol -1) in (newCoords, newCoords : coordsList)
-  | hRow < tRow && hCol > tCol = let newCoords = (tRow -1, tCol + 1) in (newCoords, newCoords : coordsList)
+      then (tRow + 1, tCol)
+      else (tRow -1, tCol)
+  | hRow > tRow && hCol > tCol = (tRow + 1, tCol + 1)
+  | hRow < tRow && hCol < tCol = (tRow -1, tCol -1)
+  | hRow > tRow && hCol < tCol = (tRow + 1, tCol -1)
+  | hRow < tRow && hCol > tCol = (tRow -1, tCol + 1)
   | otherwise = error $ "IMPOSSIBLE: Move tail. This should not happen"
+
+moveTails :: H -> [T] -> [T]
+moveTails h ts = tail $ scanl moveTail h ts
 
 distance2 :: (Int, Int) -> (Int, Int) -> Int
 distance2 (x1, y1) (x2, y2) = floor . sqrt $ (fromIntegral (x' * x' + y' * y') :: Float)
@@ -54,24 +57,27 @@ distance2 (x1, y1) (x2, y2) = floor . sqrt $ (fromIntegral (x' * x' + y' * y') :
     x' = x1 - x2
     y' = y1 - y2
 
-nextMove :: H -> T -> [Coords] -> (Direction, Int) -> ((H, T), [Coords])
-nextMove h t coordsList (_, 0) = ((h, t), coordsList)
-nextMove h t coordsList (d, n) = nextMove newH newT newCoordsList (d, n -1)
+nextMoves :: H -> [T] -> [Coords] -> (Direction, Int) -> ((H, [T]), [Coords])
+nextMoves h ts coordsList (_, 0) = ((h, ts), coordsList)
+nextMoves h ts coordsList@(c : _) (d, n) = nextMoves newH newTs newCoordsList (d, n -1)
   where
     newH = moveHead h d
-    (newT, newCoordsList) = moveTail newH t coordsList
+    newTs = moveTails newH ts
+    lastTail = last newTs
+    newCoordsList = if (lastTail) == c then coordsList else (lastTail : coordsList)
+nextMoves _ _ [] _ = error $ "IMPOSSIBLE: Coordlist can not be empty"
 
-moveRope :: H -> T -> [Coords] -> [(Direction, Int)] -> ((H, T), [Coords])
-moveRope h t coordsList [] = ((h, t), coordsList)
-moveRope h t coordsList (move : xs) = moveRope newH newT newCoordsList xs
+moveRope :: H -> [T] -> [Coords] -> [(Direction, Int)] -> ((H, [T]), [Coords])
+moveRope h ts coordsList [] = ((h, ts), coordsList)
+moveRope h ts coordsList (move : xs) = moveRope newH newTs newCoordsList xs
   where
-    ((newH, newT), newCoordsList) = nextMove h t coordsList move
+    ((newH, newTs), newCoordsList) = nextMoves h ts coordsList move
 
 ex1 :: String -> Int
-ex1 = length . (S.fromList) . snd . moveRope (0, 0) (0, 0) [(0, 0)] . map parseMove . lines
+ex1 = length . (S.fromList) . snd . moveRope (0, 0) [(0, 0)] [(0, 0)] . map parseMove . lines
 
 ex2 :: String -> Int
-ex2 x = 0
+ex2 = length . (S.fromList) . snd . moveRope (0, 0) (take 9 $ repeat (0, 0)) [(0, 0)] . map parseMove . lines
 
 run :: String -> IO ()
 run x = putStr $ formatResults (ex1 x) (ex2 x)
