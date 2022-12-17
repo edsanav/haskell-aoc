22e8bff4580963695996694d573d610e26993b2c
Eduardo Santamaria <edsanav@gmail.com>
2022-12-17 20:39:46 +0100

    Day 13 parsing function

--- src/AOC2022/Day13.hs
+++ src/AOC2022/Day13.hs
@@ -4,6 +4,9 @@ import Utils (formatResults)
 
 
 import Data.Aeson
+import  Data.Maybe
+import qualified Data.Vector as V
+import qualified Data.Scientific as Sc
 
 import qualified Data.ByteString.Lazy.UTF8 as BLU -- from utf8-string
 
@@ -11,23 +14,17 @@ import qualified Data.ByteString.Lazy.UTF8 as BLU -- from utf8-string
 -- https://stackoverflow.com/questions/52218662/parse-json-rose-tree-with-haskell-aeson
 -- https://stackoverflow.com/a/74324311
 -- https://www.fpcomplete.com/haskell/library/aeson/
---data RTree a = Leaf a | Node [RTree a] deriving Show
---
---type Packet = RTree Int
---
---
-data RTree a = Leaf a | Node [RTree a] deriving Show
+
+data RTree a = Leaf a | Node [RTree a] deriving (Show)
 
 
--- TODO: Convert value to RTree (check fromJSON to convert array to list and Number
 readValue::String -> Value
-readValue x  = vInJSON
-  where vInJSON = case (decode $ BLU.fromString x) of 
-                   Just myVal -> myVal::Value
-                   Nothing -> error "Impossible"
-              
-bla:: Maybe a -> a
-bla (Just x) = x
+readValue = fromJust.decode .BLU.fromString
+
+toTree::Value -> RTree Int
+toTree (Array a) = Node $ map toTree $ V.toList a
+toTree (Number n) = Leaf $ fromIntegral (Sc.coefficient n)
+toTree x = error $ "Invalid JSON type" ++ show x
 
 ex1:: String -> Int
 ex1 x = 1
