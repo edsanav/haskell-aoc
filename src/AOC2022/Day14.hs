a288893f191d653b17d1c1abb12ea525962cfb63
Eduardo Santamaria <edsanav@gmail.com>
2023-01-09 19:45:04 +0100

    Day14, part1

--- src/AOC2022/Day14.hs
+++ src/AOC2022/Day14.hs
@@ -1,6 +1,6 @@
 module AOC2022.Day14 (run) where
 
-import Data.List (sortBy)
+import Data.List (minimumBy)
 import Data.List.Split (splitOn)
 import qualified Data.Set as S
 import Debug.Trace (traceShow)
@@ -12,10 +12,10 @@ parseRockblock :: String -> S.Set Coord
 parseRockblock = parseSection . splitOn "->"
   where
     parseSection [] = S.empty
-    parseSection (_ : []) = S.empty
+    parseSection [_] = S.empty
     parseSection (x1 : x2 : xs) = S.union (drawRockLine (getCoords x1) (getCoords x2)) (parseSection (x2 : xs))
     getCoords = toTuple . map (read :: String -> Int) . splitOn ","
-    drawRockLine (x1, y1) (x2, y2) = S.fromList [(x, y) | x <- (genList x1 x2), y <- genList y1 y2]
+    drawRockLine (x1, y1) (x2, y2) = S.fromList [(x, y) | x <- genList x1 x2, y <- genList y1 y2]
 
 
 nextObstacleInColumn :: Coord -> S.Set Coord -> Maybe Coord
@@ -23,21 +23,29 @@ nextObstacleInColumn (x0, y0) grid = nextFromTop $ S.filter (\(x, y) -> x == x0 
   where
     nextFromTop coords
       | S.null coords = Nothing
-      | otherwise = Just $ head . sortBy (\(_, y1) (_, y2) -> compare y1 y2) $ S.toList coords
+      | otherwise = Just $ minimumBy (\(_, y1) (_, y2) -> compare y1 y2) $ S.toList coords
 
 potentialNextPosition :: Coord -> S.Set Coord -> Maybe Coord
-potentialNextPosition c grid = case (nextObstacleInColumn c) grid of
+potentialNextPosition c grid = case nextObstacleInColumn c grid of
   Nothing -> Nothing
   Just (x1,y1) | not $ S.member (x1-1,y1)  grid -> potentialNextPosition (x1-1, y1) grid -- left side free
   Just (x1,y1) | not $ S.member (x1+1,y1)  grid -> potentialNextPosition (x1+1, y1) grid -- right side free
   Just c1 | c == c1 -> error "THIS SHOULD NOT HAPPEN"
-  Just (x1,y1) -> Just (x1, y1-1) 
-  
-  
+  Just (x1,y1) -> Just (x1, y1-1)
+
+dropSand:: Coord -> S.Set Coord -> Int
+dropSand orig grid = go grid 0
+  where
+    go currentGrid acc = case potentialNextPosition orig currentGrid of
+      Nothing -> acc
+      Just newPoint -> go (S.insert newPoint currentGrid) acc + 1
+
+
 ex1 :: String -> Int
-ex1 x = traceShow (potentialNextPosition (500,0) $ S.union extra grid) $ 0
+ex1 x = dropSand (500,0) grid
   where grid = S.unions . map parseRockblock $ lines x
         extra = S.fromList [(500,8), (499,8), (501,8)]
+        test = potentialNextPosition (500,0) $ S.union extra grid
 
 ex2 :: String -> Int
 ex2 _ = 0
