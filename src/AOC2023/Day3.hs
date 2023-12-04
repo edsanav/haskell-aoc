df26eda7b2727b418e0f9274a054b3afed01d5c6
Eduardo Santamaria <edsanav@gmail.com>
2023-12-04 18:13:32 +0100

    Day 3

--- src/AOC2023/Day3.hs
+++ src/AOC2023/Day3.hs
@@ -2,7 +2,7 @@ module AOC2023.Day3 (run) where
 
 import Utils (formatResults, indexRows)
 import qualified Data.Map as M
-import Data.List
+import Data.List(sort)
 import Data.Char
 
 data Position = Position {val::String, coords::[(Int,Int)]} deriving (Show)
@@ -11,11 +11,11 @@ instance Eq Position where
   p1 == p2 = sort (coords p1) == sort (coords p2)
 
 type Ref = M.Map (Int,Int) Char
-type Size = (Int, Int)
 
 loadRef::String -> Ref
 loadRef input = M.fromList $ foldMap addCols  $ indexRows input
   where addCols (row, line) = zipWith (\col ch -> ((row, col), ch)) ([0..]::[Int]) line
+
 getNeighboursCoords:: Ref -> (Int, Int) -> [(Int, Int)]
 getNeighboursCoords ref (row, col) = [(i,j) | i <- [row-1..row+1],
                                         j <- [col-1..col+1],
@@ -27,39 +27,48 @@ getNeighbourVals:: Ref -> (Int, Int) -> [Char]
 getNeighbourVals ref = map (ref M.!) . getNeighboursCoords ref
 
 adjacentSymbol::Ref -> (Int, Int) -> Bool
-adjacentSymbol ref coords = any (\x -> not (isDigit x) && (x /='.')) $ getNeighbourVals ref coords
+adjacentSymbol ref cs = any (\x -> not (isDigit x) && (x /='.')) $ getNeighbourVals ref cs
 
-nums:: String -> [(String, [Int])]
-nums line = go 0 []
-  where digits = M.fromList $ filter (isDigit.snd) $ zip [(0::Int)..] line
-        go n accum
-          | n >= length line  = reverse accum
-          | M.notMember n digits = go (n+1) accum
-          | otherwise = if M.member (n-1) digits
-                        then case accum of
-                                (numStr, indexes):xs -> go (n+1) $ (numStr++[digits M.! n], indexes ++ [n]):xs
-                                [] -> error "This should not happen"
-                        else go (n+1) $ ([digits M.! n], [n]):accum 
+finishNum::Ref -> Position -> Position 
+finishNum ref pos@(Position posStr cs) 
+  | nextIsDigit = finishNum ref (Position (posStr ++ [nextRowVal]) (cs ++ [nextRowPos]) )
+  | otherwise = pos   
+  where (lastRow, lastCol) = last cs
+        nextRowPos = (lastRow, lastCol + 1)
+        nextIsDigit = M.member (lastRow, lastCol + 1 ) ref  && isDigit (ref M.! (lastRow, lastCol + 1))
+        nextRowVal = ref M.! nextRowPos
 
+findNums::Ref -> [Position]
+findNums ref = [finishNum ref (Position [ch] [cords]) | (cords@(row, col), ch) <- M.toList ref,
+                                                        isDigit ch,
+                                                        M.notMember (row, col-1) ref || not (isDigit (ref M.! (row, col-1)))
+                                                        ]
 
+isNearSymbol::Ref -> Position -> Bool
+isNearSymbol ref pos = any (adjacentSymbol ref)  (coords pos)
 
---nums:: String -> [[(Char, Int)]]
---nums line = undefined
---  where digits = M.fromList $ filter (isDigit.snd) $ zip [(0::Int)..] line
---        result = foldr []
+-- Not the most efficient implementation at all
+getAdjacentNums:: (Int,Int) -> [Position] -> [Position]
+getAdjacentNums (row,col) positions = go positions []
+  where neighCoords = [(i,j) | i <- [row-1..row+1], j <- [col-1..col+1]]
+        isNeighbour (Position _ cs) = any (`elem` neighCoords) cs
+        go [] accum = accum
+        go (pos:xs) accum
+          | isNeighbour pos = go xs (pos:accum)
+          | otherwise = go xs accum
 
-
-getLimits::String -> (Int,Int)
-getLimits input = (rows-1, cols-1)
-  where ls = lines input
-        rows = length ls
-        cols = length (head ls)
 
 ex1 :: String -> Int
-ex1 = undefined
+ex1 input = sum $ map (read.val) $ filter (isNearSymbol ref) nums
+  where ref = loadRef input
+        nums = findNums ref
 
 ex2 :: String -> Int
-ex2 = undefined
+ex2 input = sum $ map (product.map (read.val)) gearsNums
+  where ref = loadRef input
+        candidates = M.filter (=='*') ref
+        nums = findNums ref
+        gearsNums = filter ((==2).length) $ map (`getAdjacentNums` nums) (M.keys candidates)
 
 run :: String -> IO ()
 run x = putStr $ formatResults (ex1 x) (ex2 x)
