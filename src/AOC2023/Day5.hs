f7ad8e99c10414664ab0134efa8756bb7c178a48
Eduardo Santamaria <edsanav@gmail.com>
2023-12-13 19:58:27 +0100

    Day5, and cleaned up

--- src/AOC2023/Day5.hs
+++ src/AOC2023/Day5.hs
@@ -6,34 +6,33 @@ import Data.List.Split (splitOn)
 readSeeds:: String -> [Int]
 readSeeds = map read.tail.words
 
-readSeedRanges::String -> [Int]
-readSeedRanges = readRange . readSeeds
-  where readRange (x:y:xs) = [x..(x+y-1)] ++ readRange xs 
-        readRange [] = []
-        readRange [_] = error "this should not happen"
 
-readBlock::String -> Int -> Int
-readBlock input = createMapFun readNumbersInput
-  where 
-    toNumbersInput (dest:source:interval:_) = (dest,source,interval)
-    toNumbersInput _ = error "this should not happen"
-    readNumbersInput = map (toNumbersInput . map (read::String->Int) . words) $  tail.lines $ input
-    createMapFun [] x = x 
-    createMapFun ((dest, source, len):xs) x = if (source <= x) && x <= source+(len-1) 
-                                              then dest+(x-source)
-                                              else createMapFun xs x
+
+readMappings::String -> [(Int,Int,Int)]
+readMappings input = map (toNumbersInput . map (read::String->Int) . words) $  tail.lines $ input
+   where
+     toNumbersInput (dest:source:interval:_) = (dest,source,interval)
+     toNumbersInput _ = error "this should not happen"
+
+directFunc::[(Int,Int,Int)] -> (Int -> Int)
+directFunc [] x = x
+directFunc ((dest, source, len):xs) x = if (source <= x) && x <= source+(len-1) 
+                                          then dest+(x-source)
+                                          else directFunc xs x
+
 -- y - dest + source  = x
-readBlockReverse::String -> Int -> Int
-readBlockReverse input = createMapFun readNumbersInput
-  where
-    toNumbersInput (dest:source:interval:_) = (dest,source,interval)
-    toNumbersInput _ = error "this should not happen"
-    readNumbersInput = map (toNumbersInput . map (read::String->Int) . words) $  tail.lines $ input
-    createMapFun [] x = x 
-    createMapFun ((dest, source, len):xs) y = let x = y - dest + source
-                                              in if (source <= x) && x <= source+(len-1) 
-                                                 then x
-                                                 else createMapFun xs y
+reverseFunc::[(Int,Int,Int)] -> (Int -> Int)
+reverseFunc [] y = y
+reverseFunc ((dest, source, len):xs) y = let x = y - dest + source
+                                         in if (source <= x) && x <= source+(len-1)
+                                            then x
+                                            else reverseFunc xs y
+                                            
+isValidOutput::String -> Int -> Bool
+isValidOutput = readRange.readSeeds
+  where readRange (start:amount:xs) x = (start <= x) && (x <= (start+amount-1)) || readRange xs x
+        readRange [] _ = False
+        readRange [_] _ = error "this should not happen"
 
 
 composeFinalFunction::[Int -> Int] -> Int -> Int
@@ -42,13 +41,13 @@ composeFinalFunction = foldl (flip (.)) id
 ex1 :: String -> [String] -> Int
 ex1 seedline blocksStr = minimum $ map finalFunc seeds
   where seeds = readSeeds seedline
-        finalFunc = composeFinalFunction $ map readBlock blocksStr
+        finalFunc = composeFinalFunction $ map (directFunc.readMappings) blocksStr
 
 ex2 :: String -> [String]  -> Int
-ex2 seedline blocksStr = finalFunc 46
---ex2 seedline blocksStr = minimum $ map finalFunc seeds
-  where seeds = readSeedRanges seedline
-        finalFunc = composeFinalFunction $ reverse $ map readBlockReverse blocksStr
+ex2 seedline blocksStr = head valid
+  where validOutputFunc = isValidOutput seedline
+        finalFunc = composeFinalFunction $ reverse $ map (reverseFunc.readMappings) blocksStr
+        (_, valid) = break (validOutputFunc.finalFunc) [0..]
 
 
 run :: String -> IO ()
