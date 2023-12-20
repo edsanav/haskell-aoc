8b7561bfadfde02e78bcc21bcb511588242f9610
Eduardo Santamaria <edsanav@gmail.com>
2023-12-20 19:30:42 +0100

    Day7, part1, working for example only

--- src/AOC2023/Day7.hs
+++ src/AOC2023/Day7.hs
@@ -1,7 +1,7 @@
 module AOC2023.Day7 (run) where
 
-import Utils (formatResults)
 import Data.List
+import Utils (formatResults)
 
 -- Notice this implies CA < C2 !
 data Card = CA | CK | CQ | CJ | CT | C9 | C8 | C7 | C6 | C5 | C4 | C3 | C2 deriving (Show, Eq, Ord)
@@ -26,22 +26,30 @@ readCard c = case c of
   '3' -> C3
   '2' -> C2
   x -> error $ "Invalid card " ++ [x]
-  
-readHand:: [Card] -> Hand
-readHand cards = undefined
-  where grouped = group.sort $ cards
-        grouped 
-          | 
+
+readHand :: [Card] -> Hand
+readHand cards = hand
+  where
+    grouped = group . sort $ cards
+    maxRepetitions = maximum . map length
+    hand = case maxRepetitions grouped of
+      5 -> Five cards
+      4 -> Four cards
+      3 | length grouped == 2 -> FullHouse cards
+      3 -> Three cards
+      2 -> Two cards
+      1 -> HighCard cards
+      _ -> error $ "Invalid hand cards " ++ show cards
 
 parseLine :: String -> (Hand, Bid)
-parseLine line = undefined
-  where
-    (handContent, bid) = case words line of
-      [handStr, bidStr] -> (map readCard handStr, read bidStr :: Int)
-      _ -> error "Invalid line input"
+parseLine line = case words line of
+  [handStr, bidStr] -> (readHand $ map readCard handStr, read bidStr :: Int)
+  _ -> error "Invalid line input"
 
 ex1 :: String -> Int
-ex1 = undefined
+ex1 input = sum $ map (uncurry (*))  $ zipped input
+  where 
+    zipped inp = zip [1..] .map snd.reverse.sort.map parseLine $ lines inp :: [(Int, Bid)]
 
 ex2 :: String -> Int
 ex2 = undefined
