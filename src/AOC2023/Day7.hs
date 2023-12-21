16452c33e119a322c3ddb3516b15ff515a98d641
Eduardo Santamaria <edsanav@gmail.com>
2023-12-21 20:13:43 +0100

    Day 7

--- src/AOC2023/Day7.hs
+++ src/AOC2023/Day7.hs
@@ -4,18 +4,20 @@ import Data.List
 import Utils (formatResults)
 
 -- Notice this implies CA < C2 !
-data Card = CA | CK | CQ | CJ | CT | C9 | C8 | C7 | C6 | C5 | C4 | C3 | C2 deriving (Show, Eq, Ord)
+data Mode = Original | WithJoker deriving (Show, Eq)
 
-data Hand = Five [Card] | Four [Card] | FullHouse [Card] | Three [Card] | Two [Card] | HighCard [Card] deriving (Show, Eq, Ord)
+data Card = CA | CK | CQ | CJ | CT | C9 | C8 | C7 | C6 | C5 | C4 | C3 | C2 | Joker deriving (Show, Eq, Ord)
+
+data Hand = Five [Card] | Four [Card] | FullHouse [Card] | Three [Card] | TwoPairs [Card] | OnePair [Card] | HighCard [Card] deriving (Show, Eq, Ord)
 
 type Bid = Int
 
-readCard :: Char -> Card
-readCard c = case c of
+readCard :: Mode -> Char -> Card
+readCard jValue c = case c of
   'A' -> CA
   'K' -> CK
   'Q' -> CQ
-  'J' -> CJ
+  'J' -> if jValue == Original then CJ else Joker
   'T' -> CT
   '9' -> C9
   '8' -> C8
@@ -37,24 +39,54 @@ readHand cards = hand
       4 -> Four cards
       3 | length grouped == 2 -> FullHouse cards
       3 -> Three cards
-      2 -> Two cards
+      2 | length grouped == 3 -> TwoPairs cards
+      2 -> OnePair cards
       1 -> HighCard cards
       _ -> error $ "Invalid hand cards " ++ show cards
 
-parseLine :: String -> (Hand, Bid)
-parseLine line = case words line of
-  [handStr, bidStr] -> (readHand $ map readCard handStr, read bidStr :: Int)
+bestHand :: [Card] -> Hand
+bestHand cards = transplantHand cards $ recreateHand cards
+  where
+    -- Probably there is a nicer way of doing this transition
+    transplantHand cs (Five _) = Five cs
+    transplantHand cs (Four _) = Four cs
+    transplantHand cs (FullHouse _) = FullHouse cs
+    transplantHand cs (Three _) = Three cs
+    transplantHand cs (TwoPairs _) = TwoPairs cs
+    transplantHand cs (OnePair _) = OnePair cs
+    transplantHand cs (HighCard _) = HighCard cs
+    --
+    expandCard :: Card -> [Card]
+    expandCard card = case card of
+      Joker -> [CA, CK, CQ, CT, C9, C8, C7, C6, C5, C4, C3, C2]
+      c -> [c]
+    --
+    recreateHand [a, b, c, d, e] =
+      minimum $
+        [ readHand [c1, c2, c3, c4, c5]
+          | c1 <- expandCard a,
+            c2 <- expandCard b,
+            c3 <- expandCard c,
+            c4 <- expandCard d,
+            c5 <- expandCard e
+        ]
+    recreateHand _ = error "not possible"
+
+parseLine :: Mode -> String -> (Hand, Bid)
+parseLine mode line = case (mode, words line) of
+  (Original, [handStr, bidStr]) -> (readHand $ map (readCard mode) handStr, read bidStr :: Int)
+  (WithJoker, [handStr, bidStr]) -> (bestHand $ map (readCard mode) handStr, read bidStr :: Int)
   _ -> error "Invalid line input"
 
 ex1 :: String -> Int
-ex1 input = sum $ map (uncurry (*))  $ zipped input
-  where 
-    zipped inp = zip [1..] .map snd.reverse.sort.map parseLine $ lines inp :: [(Int, Bid)]
+ex1 input = sum $ map (uncurry (*)) $ zipped input
+  where
+    zipped inp = zip [1 ..] . map snd . reverse . sort . map (parseLine Original) $ lines inp :: [(Int, Bid)]
 
 ex2 :: String -> Int
-ex2 = undefined
+ex2 input = sum $ map (uncurry (*)) $ zipped input
+  where
+    zipped inp = zip [1 ..] . map snd . reverse . sort . map (parseLine WithJoker) $ lines inp :: [(Int, Bid)]
 
 run :: String -> IO ()
 run x = putStr $ formatResults (ex1 x) (ex2 x)
-
--- TODO include one pair
